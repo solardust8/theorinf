0d51a983461ea7ae031731415560b553e5286822
solardust8 <egor.dom0923@gmail.com>
2024-05-23 13:33:08 +0300

    final changes

--- ready_model_demo.py
+++ ready_model_demo.py
@@ -16,6 +16,7 @@ sys.path.append(os.path.join(os.getcwd(), 'src'))
 TEST_PATH = os.path.join('.', 'data', 'test')
 DEVICE = 'cuda:0' if torch.cuda.is_available() else 'cpu'
 WEIGHTS = os.path.join('.', 'weights', 'RAE_MISH_16x16x16latent_200epochs.ckpt')
+WEIGHTS_base = os.path.join('.', 'weights', 'base_ae.pth')
 
 test_dataset = ImageDataset(TEST_PATH)
 test_loader = DataLoader(test_dataset, batch_size=16, shuffle=False)
@@ -27,11 +28,12 @@ bt = 3
 
 
 MODEL = ResAE(activation=nn.Mish()).to(DEVICE)
+#MODEL = BaseAutoEncoder().to(DEVICE)
 MODEL.load_state_dict(torch.load(WEIGHTS, map_location=torch.device(DEVICE)))
 MODEL.eval()
 
 import faulthandler; faulthandler.enable()
 
 imgs_decoded, imgsQ_decoded, bpp = process_images(test_loader, MODEL, DEVICE, b, w, h)
-display_images_and_save_pdf(test_dataset, imgs_decoded, imgsQ_decoded, bpp, './misc/out3.jpg', NumImagesToShow=5)
+display_images_and_save_pdf(test_dataset, imgs_decoded, imgsQ_decoded, bpp, './misc/out1.jpg', NumImagesToShow=5)
 
--- src/ResAE.py
+++ src/ResAE.py
@@ -59,3 +59,42 @@ class ResAE(nn.Module):
             x = x + noise
         x = self.decoder(x)
         return x
+    
+class BaseAutoEncoder(nn.Module):
+    def __init__(self):
+        super(BaseAutoEncoder, self).__init__()
+        self.encoder = nn.Sequential(
+            nn.Conv2d(3, 128, kernel_size=7, padding=3),
+            nn.ReLU(),
+            nn.MaxPool2d(2),
+            nn.Conv2d(128, 32, kernel_size=5, padding=2),
+            nn.ReLU(),
+            nn.MaxPool2d(2),
+            nn.Conv2d(32, 16, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(2),
+        )
+
+        self.decoder = nn.Sequential(
+            nn.ConvTranspose2d(
+                16, 32, kernel_size=3, stride=2, padding=1, output_padding=1
+            ),
+            nn.ReLU(),
+            nn.ConvTranspose2d(
+                32, 128, kernel_size=5, stride=2, padding=2, output_padding=1
+            ),
+            nn.ReLU(),
+            nn.ConvTranspose2d(
+                128, 3, kernel_size=7, stride=2, padding=3, output_padding=1
+            ),
+            nn.Sigmoid(),
+        )
+
+    def forward(self, x, b_t=None):
+        x = self.encoder(x)
+        if self.training and b_t is not None:
+            max_val = x.max() / (2 ** (b_t + 1))
+            noise = torch.rand_like(x) * max_val
+            x = x + noise
+        x = self.decoder(x)
+        return x
